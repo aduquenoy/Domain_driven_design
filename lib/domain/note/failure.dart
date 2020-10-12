c76c6e4093656846de3225010d215b9cdef78afc
aduquenoy <59051812+aduquenoy@users.noreply.github.com>
2020-10-12 17:34:59 +0200

    ddd_17_CUD

--- lib/domain/note/failure.dart
+++ lib/domain/note/failure.dart
@@ -2,7 +2,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'failure.freezed.dart';
 
 @freezed
+// Union cases :
 abstract class NoteFailure with _$NoteFailure{
   const factory NoteFailure.unexpected() = _Unexpected;
   const factory NoteFailure.insufficientPermission() = _InsufficientPermission;
+  const factory NoteFailure.unableToUpdate() = _UnableToUpdate;
 }
